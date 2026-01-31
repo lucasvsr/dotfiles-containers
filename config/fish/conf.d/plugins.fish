7cb34401d8d4014301277483d50e1475a5122fe1
Lucas Ribeiro <lucasvsribeiro@outlook.com>
2026-01-31 06:49:27 -0300

    fix(fish): valida se existe o fisher

--- config/fish/conf.d/plugins.fish
+++ config/fish/conf.d/plugins.fish
@@ -1,6 +1,6 @@
 function _install_plugins --on-event fish_prompt
     
-    if ! test -f $HOME/.config/fish/fish_plugins.installed; and fisher list &> /dev/null;
+    if ! test -f $HOME/.config/fish/fish_plugins.installed; and type -q fisher; and fisher list &> /dev/null;
 
         fisher update && touch $HOME/.config/fish/fish_plugins.installed
         
